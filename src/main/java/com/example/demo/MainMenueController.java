d2b04504724e79e1ba45b5b6e976e4ec4ab6bcf1
Marcx <haitianhero6@gmail.com>
2022-08-27 11:54:57 -0400

    Starting commit

---
+++ src/main/java/com/example/demo/MainMenueController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenueController implements Initializable  {
+    public Label TheLabel;
+
+    public int count = 1;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("I am initialized");
+    }
+
+    public void onButtonClick(ActionEvent actionEvent) {
+        System.out.println("I am clicked");
+        TheLabel.setText("You clicked the button " + count++);
+    }
+}
