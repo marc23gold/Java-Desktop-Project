71e29af2a56c173063b693a6663912ab08ebb022
Marcx <haitianhero6@gmail.com>
2022-09-06 17:26:30 -0400

    broken code

--- src/main/java/com/example/demo/MainMenueController.java
+++ src/main/java/com/example/demo/MainMenueController.java
@@ -2,18 +2,30 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static javafx.fxml.FXMLLoader.load;
+
 public class MainMenueController implements Initializable  {
+
+    Stage stage;
+    Parent scene;
     public Label TheLabel;
 
     public int count = 1;
     @FXML
     private Label welcomeText;
+    private Parent FXMLLoader;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -30,7 +42,11 @@ public class MainMenueController implements Initializable  {
         TheLabel.setText("You clicked the button " + count++);
     }
 
-    public void onActionCreateAnimal(ActionEvent actionEvent) {
+    public void onActionCreateAnimal(ActionEvent actionEvent) throws IOException {
+        stage =(Stage)((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/resources/com/example/demo/MainMenue.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     public void onActionDisplayAnimal(ActionEvent actionEvent) {
